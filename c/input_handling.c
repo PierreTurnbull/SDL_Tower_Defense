473bd68648219dfdc7f15901a6ed93b1be89c0d7
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-01-03 15:43:51 +0100

    input_handling done.

--- c/input_handling.c
+++ c/input_handling.c
@@ -1,4 +1,13 @@
 void  input_handling(struct s_game *game)
 {
-  
+  if      (game->event.type == SDL_KEYDOWN)
+  {
+    game->input.key[game->event.key.keysym.scancode] = SDL_TRUE;
+    fprintf(stderr, "%s INPUT_HANDLING\nkey pressed: %d\n\n", get_game_time(), game->event.key.keysym.scancode);
+  }
+  else if (game->event.type == SDL_KEYUP)
+  {
+    game->input.key[game->event.key.keysym.scancode] = SDL_FALSE;
+    game->input.timer[game->event.key.keysym.scancode] = 0;
+  }
 }
