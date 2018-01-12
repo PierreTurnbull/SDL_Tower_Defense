83f4b8d8e6be865bc67d900c555dd43cc7b64a68
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-01-12 17:36:12 +0100

    reorganization of structure files done.

---
+++ h/structures/s_main.h
@@ -0,0 +1,29 @@
+#include "s_play.h"
+#include "s_main_menu.h"
+
+struct s_input
+{
+  SDL_bool            quit;
+
+  SDL_bool            key[SDL_NUM_SCANCODES];
+
+  SDL_bool            mouse_btn[3];
+  SDL_Rect            mouse_pos;
+
+  int                 timer[SDL_NUM_SCANCODES];
+};
+
+struct s_game
+{
+  SDL_Renderer        *rend;
+  SDL_Event           event;
+  SDL_Window          *wind;
+  SDL_Texture         *cursor[3];
+
+  struct s_input      input;
+  struct s_main_menu  main_menu;
+  struct s_play       play;
+  int                 screen;
+  int                 current_cursor;
+  int                 game_time;
+};
