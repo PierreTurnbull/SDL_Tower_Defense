e4940f86fba2398065b6c6ac1f59c704baa476e0
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-01-15 21:43:56 +0100

    checkpoint. load/frees to do.

--- h/structures/s_play.h
+++ h/structures/s_play.h
@@ -7,9 +7,25 @@ struct s_warzone
 
 struct s_gui
 {
-  SDL_Texture         *background;
+  SDL_Texture         *background; // TODO load free
+  SDL_Texture         *items_background; // TODO load free
+  SDL_Texture         *items_category[3]; // TODO load free
+  SDL_Texture         *items_category_hover[3]; // TODO load free
+  SDL_Texture         *gold_background; // TODO load free
+  SDL_Texture         *next_wave_background[2]; // TODO load free
+  SDL_Texture         *pause; // TODO load free
+  SDL_Texture         *options; // TODO load free
+  SDL_Texture         *menu; // TODO load free
 
-  SDL_Rect            background_pos;
+  SDL_Rect            background_pos; // TODO load
+  SDL_Rect            items_background_pos; // TODO load
+  SDL_Rect            items_category[3]_pos; // TODO load
+  SDL_Rect            items_category_hover[3]_pos; // TODO load
+  SDL_Rect            gold_background_pos; // TODO load
+  SDL_Rect            next_wave_background[2]_pos; // TODO load
+  SDL_Rect            pause_pos; // TODO load
+  SDL_Rect            options_pos; // TODO load
+  SDL_Rect            menu_pos; // TODO load
 };
 
 struct s_play
