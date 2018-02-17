22703a3a0d7d1d6af47988abf62c0f4a0bb87eea
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-02-17 12:41:28 +0100

    create target becomes 3 fts.

--- c/handle_event/create/create_target.c
+++ c/handle_event/create/create_target.c
@@ -1,27 +1,5 @@
-void  create_target(struct s_game *game, int type)
+void  fill_target(struct s_game *game, struct s_target *new_target, int type)
 {
-  int i;
-  struct s_target *new_target = malloc(sizeof(*new_target));
-  struct s_target *ptr;
-
-  i = 0;
-  ptr = game->play.target_list.first;
-  while (ptr != NULL) {
-    ptr = ptr->next;
-    i++;
-  }
-  if (i == 0) {
-    game->play.target_list.first = new_target;
-    new_target->prev = NULL;
-  } else {
-    ptr = game->play.target_list.first;
-    while (ptr->next != NULL) {
-      ptr = ptr->next;
-    }
-    new_target->prev = ptr;
-    ptr->next = new_target;
-  }
-  new_target->index           = i;
   new_target->type            = type;
   new_target->move_speed      = game->play.target_list.target_move_speed[type];
   new_target->pos.x           = game->play.path.first->pos.x - game->play.target_list.target_size[type] / 2;
@@ -42,5 +20,38 @@ void  create_target(struct s_game *game, int type)
                                 (new_target->next_path_block->pos.y - game->play.path.first->pos.y) *
                                 (new_target->next_path_block->pos.y - game->play.path.first->pos.y));
   new_target->next            = NULL;
+}
+
+void  append_target(struct s_game *game, struct s_target *new_target, struct s_target *ptr)
+{
+  int i;
+
+  i = 0;
+  while (ptr != NULL) {
+    ptr = ptr->next;
+    i++;
+  }
+  if (i == 0) {
+    game->play.target_list.first = new_target;
+    new_target->prev = NULL;
+  } else {
+    ptr = game->play.target_list.first;
+    while (ptr->next != NULL) {
+      ptr = ptr->next;
+    }
+    new_target->prev = ptr;
+    ptr->next = new_target;
+  }
+  new_target->index           = i;
+}
+
+void  create_target(struct s_game *game, int type)
+{
+  struct s_target *new_target = malloc(sizeof(*new_target));
+  struct s_target *ptr;
+
+  ptr = game->play.target_list.first;
+  append_target(game, new_target, ptr);
+  fill_target(game, new_target, type);
   fprintf(stderr, "%s CREATE_TARGET\n%p\nindex           = %d\ntype            = %d\nmove_speed      = %f\npos             = x%d y%d\nvel             = x%f y%f\nnext_path_block = %p\nprev            = %p\nnext            = %p\n\n", TIME_get_time(), new_target, new_target->index, new_target->type, new_target->move_speed, new_target->pos.x, new_target->pos.y, new_target->velx, new_target->vely, new_target->next_path_block, new_target->prev, new_target->next);
 }
