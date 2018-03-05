20f54458e9039838189fbe4ea48bc735b9bbba9b
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-03-05 12:52:46 +0100

    mouse repeat handled.

--- c/handle_input.c
+++ c/handle_input.c
@@ -1,21 +1,25 @@
 void  handle_input(void)
 {
-  if (game.event.type == SDL_KEYDOWN)
-  {
+  if (game.event.type == SDL_KEYDOWN) {
     if (game.input.key[game.event.key.keysym.scancode] == SDL_TRUE)
-      game.input.repeat[game.event.key.keysym.scancode] = SDL_TRUE;
+      game.input.key_repeat[game.event.key.keysym.scancode] = SDL_TRUE;
     game.input.key[game.event.key.keysym.scancode] = SDL_TRUE;
   }
-  else if (game.event.type == SDL_KEYUP)
-  {
+  else if (game.event.type == SDL_KEYUP) {
     game.input.key[game.event.key.keysym.scancode] = SDL_FALSE;
-    game.input.timer[game.event.key.keysym.scancode] = 0;
-    game.input.repeat[game.event.key.keysym.scancode] = SDL_FALSE;
+    game.input.key_timer[game.event.key.keysym.scancode] = 0;
+    game.input.key_repeat[game.event.key.keysym.scancode] = SDL_FALSE;
+  }
+  else if (game.event.type == SDL_MOUSEBUTTONDOWN) {
+    if (game.input.mouse_btn[game.event.button.button - 1] == SDL_TRUE)
+      game.input.mouse_btn_repeat[game.event.button.button - 1] = SDL_TRUE;
+    game.input.mouse_btn[game.event.button.button - 1] = SDL_TRUE;
+  }
+  else if (game.event.type == SDL_MOUSEBUTTONUP) {
+    game.input.mouse_btn[game.event.button.button - 1] = SDL_FALSE;
+    game.input.mouse_btn_timer[game.event.button.button - 1] = 0;
+    game.input.mouse_btn_repeat[game.event.button.button - 1] = SDL_FALSE;
   }
   else if (game.event.type == SDL_MOUSEMOTION)
     SDL_GetMouseState(&game.input.mouse_pos.x, &game.input.mouse_pos.y);
-  else if (game.event.type == SDL_MOUSEBUTTONDOWN)
-    game.input.mouse_btn[game.event.button.button - 1] = SDL_TRUE;
-  else if (game.event.type == SDL_MOUSEBUTTONUP)
-    game.input.mouse_btn[game.event.button.button - 1] = SDL_FALSE;
 }
