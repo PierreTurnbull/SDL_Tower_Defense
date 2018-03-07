73796108ecb078e7ef92db018fbf96d486da3d48
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-03-07 22:28:15 +0100

    fixed SEGFAULT

--- c/handle_event/create/create_tower.c
+++ c/handle_event/create/create_tower.c
@@ -11,6 +11,7 @@ void  fill_tower(struct s_tower *new_tower, int type)
   new_tower->range_pos.y              = new_tower->pos.y - game.play.tower_list.tower_range_size[type] / 2 + new_tower->pos.h / 2;
   new_tower->range_pos.w              = game.play.tower_list.tower_range_size[type];
   new_tower->range_pos.h              = game.play.tower_list.tower_range_size[type];
+  new_tower->proj_list.first          = NULL;
 }
 
 void  append_tower(struct s_tower *new_tower)
